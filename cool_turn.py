51ffb5506a683900f312d6cc66899c2fa1b1d87e
CCscenery <48652198+CCscenery@users.noreply.github.com>
2020-12-06 16:39:50 +0800

    Add files via upload
    cool_turn 模块后端接口的编写

---
+++ cool_turn.py
@@ -0,0 +1,64 @@
+from ExcelToWord import excel_to_word
+from WordToExcel import word_to_excel
+from ProcessExcel import process_excel
+from WirteBatchWord import batch_word
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)  # 解决跨域问题
+
+def enter_choice():
+    optional = ('1', '2', '3', '4')
+    while True:
+        try:
+            print("功能选择:\n1、excel导出批量word\n2、批量word归并为excel")
+            print("3、excel多到一或一到多的转换\n4、批量写入word")
+            choice = input()
+            if choice not in optional:
+                raise ValueError('请输入"1"、"2"、"3"、"4"中的一个')
+            break
+        except Exception as err:
+            print(err)
+    return choice
+
+def enter_whether_quit():
+    optional = '是否YNyn'
+    sure = '是Yy'
+    while True:
+        try:
+            print("是否退出程序?(是/否)(y/n)")
+            choice = input()
+            if choice not in optional:
+                raise ValueError('请按照提示输入')
+            break
+        except Exception as err:
+            print(err)
+    choice = True if choice in sure else False
+    return choice
+
+def main(choice):
+    if choice == '1':  # excel导出批量word
+        excel_to_word.main()
+    elif choice == '2':  # 批量word归并为excel
+        word_to_excel.main()
+    elif choice == '3':
+        process_excel.main()
+    elif choice == '4':
+        batch_word.main()
+
+
+if __name__ == "__main__":
+    @app.route('/',methods=['POST'])
+    def home_page():
+        choice = request.get_json('data')['choice']
+        main(choice)
+
+# if __name__ == "__main__":
+#     while True:
+#         choice = enter_choice()
+#         main(choice)
+#         choice = enter_whether_quit()
+#         if choice:
+#             break
